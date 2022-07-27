aaff4d060f3d93215f2215e077f2da80d4069f54
Satendra Kumar <satendramca2016@gmail.com>
2022-07-27 23:07:03 +0530

    add code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-import Header from './Components/Header';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
    <h1>Hello World</h1>
    <Header/>
+   <Footer/>
     </div>
   );
 }
